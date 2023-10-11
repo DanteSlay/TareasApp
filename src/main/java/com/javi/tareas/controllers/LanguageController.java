a17e09a62db11b16b2b7ecaab6b6ec0c2711f99f
DanteSlay <danteslay16@gmail.com>
2023-10-11 16:01:59 +0200

    Solucionado Bug al cambiar idioma despues de submit

--- src/main/java/com/javi/tareas/controllers/LanguageController.java
+++ src/main/java/com/javi/tareas/controllers/LanguageController.java
@@ -26,6 +26,11 @@ public class LanguageController {
         // Obtiene la URL de la página anterior (Referer) para redireccionar allí
         String referer = request.getHeader("Referer");
 
+        //Si venimos de un submit erroneo volveremos a la URL del método GET
+        if (referer.contains("submit")) {
+            return "redirect:" + referer.replace("/submit", "");
+        }
+
         // Retorna una redirección a la página anterior o a la página de inicio si no hay una página anterior
         return "redirect:" + (referer != null ? referer : "/");
     }
